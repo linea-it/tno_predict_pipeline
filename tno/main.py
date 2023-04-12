d7dfbbd8e45d1f8985409e2185fe798ccf3872c2
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2023-04-12 12:09:21 -0300

    partial

--- tno/main.py
+++ tno/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from worker import create_task
+from celery.result import AsyncResult
 
 app = FastAPI()
 
@@ -19,6 +20,16 @@ def run_task(payload = Body(...)):
     task = create_task.delay(int(task_type))
     return JSONResponse({"task_id": task.id})
 
+@app.get("/tasks/{task_id}")
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JSONResponse(result)
+
 # https://testdriven.io/blog/fastapi-and-celery/
 # https://github.com/testdrivenio/fastapi-celery/blob/master/docker-compose.yml
 # https://www.toptal.com/python/orchestrating-celery-python-background-jobs
@@ -28,3 +39,4 @@ def run_task(payload = Body(...)):
 
 # Para testar: 
 # curl http://backend:8000/tasks -H "Content-Type: application/json" --data '{"type": 0}'
+# curl http://backend:8000/tasks/c02e0d7c-a326-4b41-93db-48c60ccf7a86
