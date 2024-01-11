01e18dbba1db4d43f99faf9241b5db79da9d9e62
Cristiano Singulani <crisingulani@gmail.com>
2024-01-11 17:45:24 -0300

    Added checksum in pipeline

--- daemon.sh
+++ daemon.sh
@@ -5,6 +5,14 @@ set -o pipefail
 
 source /app/src/env.sh
 
+MD5_ORIGIN=`find /predict_occultation/pipeline -type f -exec md5sum {} \; | sort -k 2 | md5sum`
+MD5_PIPE=`find ${PIPELINE_PATH} -type f -exec md5sum {} \; | sort -k 2 | md5sum`
+
+if [[ "$MD5_ORIGIN" != "$MD5_PIPE" ]]; then
+    echo "${PIPELINE_PATH}: directory content does not correspond to the container, copying container content..."
+    cp -r /predict_occultation/pipeline/* ${PIPELINE_PATH}/
+fi
+
 # while true; do sleep 10; COUNT=$((COUNT + 10)); echo $COUNT; done;
 #bash -c 'python run_daemon.py; exit' &
 
